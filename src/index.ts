e50d05d76b35de5a0ebcf6a73e06e71257a6919b
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2024-01-28 09:34:04 +0530

    Server Initialization

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("hello world");
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compression from 'compression';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+    credentials:true
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server= http.createServer(app);
+server.listen(1000, () => {
+    console.log("Server running on > http://localhost:1000/ ");
+})
+
+app.get('/get', (req, res) => {
+    res.send('POST request to the homepage')
+})
